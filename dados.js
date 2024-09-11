2925dd1a53f4876885ef4a9e9774eb45ac6ad67b
Raphael Marchiori <102882822+MrVancouver@users.noreply.github.com>
2024-09-11 17:36:16 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,38 @@
+export let dados = [
+    {
+        titulo : "Bonecas de Pano",
+        descricao : "Bonecas de pano possuem um encanto especial, capaz de despertar a nostalgia e o afeto em pessoas de todas as idades. Feitas à mão, cada boneca carrega consigo a história e o carinho de quem a criou. A maciez do tecido, a delicadeza dos detalhes e a expressão única de cada rosto fazem das bonecas de pano peças únicas e atemporais.",
+        link : "https://www.instagram.com/p/C_XtYQpxarM/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    },        
+    {
+        titulo : "Bonecas de Biscuit",
+        descricao : "Bonecas de biscuit são verdadeiras obras de arte, moldadas com as mãos e pintadas com detalhes minuciosos. A versatilidade do biscuit permite criar bonecas com as mais diversas características, desde bebês realistas até personagens de contos de fadas. Cada peça é única e carrega consigo a personalidade de quem a criou.",
+        link : "https://www.instagram.com/p/C_Xsh47xBiO/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    },        
+    {
+        titulo : "Bolsas Personalizadas",
+        descricao : "As bolsas personalizadas são muito mais do que acessórios, são expressões da sua personalidade. Feitas à mão com materiais de alta qualidade, nossas bolsas são únicas e exclusivas. Escolha o modelo, a cor e os detalhes que mais combinam com você e tenha uma peça única e especial.",
+        link : "https://www.instagram.com/reel/C_ltc4sRpt4/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    },
+    {
+        titulo : "Almofadas Infantis",
+        descricao : "As almofadas infantis são muito mais do que um simples objeto de decoração, são companhias de aventuras e sonhos doces. Feitas com tecidos macios e delicados, cada almofada é única e especial, combinando cores vibrantes e estampas divertidas. Seja para decorar o quarto do bebê ou para tornar as brincadeiras ainda mais alegres, nossas almofadas infantis são perfeitas para acompanhar o crescimento da criançada.",
+        link: "https://www.instagram.com/p/C_XtokRR2Xu/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    },
+    {
+        titulo : "Enxoval Pré-Natal",
+        descricao : "O enxoval de bebê é muito mais do que um conjunto de peças, é o início de uma história de amor. Cada item é cuidadosamente escolhido para proporcionar conforto e aconchego ao seu bebê. Feito com materiais suaves e delicados, nosso enxoval é perfeito para os primeiros meses do seu filho(a). Descubra nossas opções de kits completos e monte o enxoval dos sonhos para o seu bebê.",
+        link : "https://www.instagram.com/p/C_Xsw0sRjiV/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    },
+    {
+        titulo : "Fantasias",
+        descricao : "Criadas à mão com tecidos nobres e detalhes minuciosos, nossas fantasias são verdadeiras obras de arte, feitas sob medida para você. Escolha o personagem, a época e o estilo que mais te representam e transforme-se em um ser mágico e único. Seja você uma princesa medieval, uma heroína futurista ou um personagem de sua própria criação, nossas fantasias te levarão para um mundo de sonhos e aventuras.",
+        link : "https://www.instagram.com/p/C_W3gJqRRba/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    }
+
+];
+export let bonecas = {
+    titulo : "Bonecas de Pano",
+    descricao : "Bonecas de pano possuem um encanto especial, capaz de despertar a nostalgia e o afeto em pessoas de todas as idades. Feitas à mão, cada boneca carrega consigo a história e o carinho de quem a criou. A maciez do tecido, a delicadeza dos detalhes e a expressão única de cada rosto fazem das bonecas de pano peças únicas e atemporais.",
+    link : "https://www.instagram.com/p/C_XtYQpxarM/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+}
